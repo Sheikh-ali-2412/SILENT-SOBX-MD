33e8e43c9d4606f13d0777fd9054574ba1b3a9a6
Sheikh-ali-2412 <aliahmad747426@gmail.com>
2024-12-07 16:30:21 +0500

    Add files via upload

--- plugins/gitstalk.js
+++ plugins/gitstalk.js
@@ -12,7 +12,7 @@
 ─██░░░░░░░░░░██──██░░░░░░██──██░░░░░░░░░░██──██░░░░░░░░░░██──██░░██──██████████░░██──────██░░██──────██░░░░░░░░░░██──██░░░░░░░░░░██────████░░████────██░░░░░░░░░░██──██░░██──██░░░░░░██─ 
 ─██████████████──██████████──██████████████──██████████████──██████──────────██████──────██████──────██████████████──██████████████──────██████──────██████████████──██████──██████████─ 
 ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────── 
-created by Silent_lover432 🕵
+created by 𓄂𝕚𝕥𝕩.𝑺𝑯𝑬𝑰𝑲𝑯 𝑨𝑳𝑰 🔥༽༼ 🕵
 contact me 923096287432 ♻️
 © Copy coder alert ⚠
 */
@@ -20,4 +20,53 @@ contact me 923096287432 ♻️
 
 
 
-function _0x54d8(){const _0x1ad209=['Please\x20provide\x20a\x20GitHub\x20username.','https://api.github.com/users/','Error\x20fetching\x20data🤕:\x20','\x0a\x0a*│\x20\x20◦*\x20🔗\x20*ɢɪᴛʜᴜʙ\x20ᴜʀʟ*:(','public_gists','../command','log','other','1248456aQIoWF','location','login','\x0a*╭┈───────────────•*\x0a*│\x20\x20◦*\x20👨‍💻*SILENT-SOBX-MD\x20GITSTALK*👨‍💻\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x0a*│\x20\x20◦*\x20👤\x20*ᴜꜱᴇʀ\x20ɴᴀᴍᴇ*:\x20','axios','../config','followers','1895788vUQiyp','6JVVeSp','message','\x20|\x20Following:\x20','get','645780tEXmOD','45SbuSSp','558242cOutAR','avatar_url','name','sendMessage','public_repos','toDateString','\x0a\x0a*│\x20\x20◦*\x20📊\x20*ᴘᴜʙʟɪᴄ\x20ʀᴇᴘᴏ*:\x20','\x0a\x0a*│\x20\x20◦*\x20👥\x20*ꜰᴏʟʟᴏᴡᴇʀꜱ*:\x20','5940710aAmbWV','20mdTJpA','Unknown','response','\x0a\x0a*│\x20\x20◦*\x20📅\x20*ᴄʀᴇᴀᴛʀᴅ\x20ᴅᴀᴛᴇ*:\x20','8874BofRSm','4143076nGqtic','\x0a\x0a*│\x20\x20◦*\x20🔭\x20*ᴘᴜʙʟɪᴄ\x20ɢɪꜱᴛꜱ*:\x20','following','data','Fetch\x20detailed\x20GitHub\x20user\x20profile\x20including\x20profile\x20picture.'];_0x54d8=function(){return _0x1ad209;};return _0x54d8();}const _0x7aaa28=_0x515a;function _0x515a(_0x3b520b,_0x104650){const _0x54d81e=_0x54d8();return _0x515a=function(_0x515a62,_0x47e736){_0x515a62=_0x515a62-0x115;let _0x3a0244=_0x54d81e[_0x515a62];return _0x3a0244;},_0x515a(_0x3b520b,_0x104650);}(function(_0x25b707,_0x18eb52){const _0x2eb844=_0x515a,_0x361a80=_0x25b707();while(!![]){try{const _0x49b233=parseInt(_0x2eb844(0x13b))/0x1+-parseInt(_0x2eb844(0x13d))/0x2*(parseInt(_0x2eb844(0x137))/0x3)+-parseInt(_0x2eb844(0x136))/0x4+-parseInt(_0x2eb844(0x11d))/0x5*(parseInt(_0x2eb844(0x121))/0x6)+-parseInt(_0x2eb844(0x122))/0x7+parseInt(_0x2eb844(0x12f))/0x8*(parseInt(_0x2eb844(0x13c))/0x9)+parseInt(_0x2eb844(0x11c))/0xa;if(_0x49b233===_0x18eb52)break;else _0x361a80['push'](_0x361a80['shift']());}catch(_0x52ee6c){_0x361a80['push'](_0x361a80['shift']());}}}(_0x54d8,0x5f413));const axios=require(_0x7aaa28(0x133)),config=require(_0x7aaa28(0x134)),{cmd,commands}=require(_0x7aaa28(0x12c));cmd({'pattern':'githubstalk','desc':_0x7aaa28(0x126),'category':_0x7aaa28(0x12e),'react':'📚','filename':__filename},async(_0x5246ba,_0xdc92af,_0x2fdffb,{from:_0x2b945c,quoted:_0x592373,body:_0x28dfdc,isCmd:_0xda7045,command:_0x3585d7,args:_0x1a4278,q:_0x567e5a,isGroup:_0x276edc,sender:_0x340cab,senderNumber:_0x18000a,botNumber2:_0x2c3e33,botNumber:_0x24622a,pushname:_0x3100d3,isMe:_0x4640e1,isOwner:_0x1fda87,groupMetadata:_0xc9f354,groupName:_0x451a4c,participants:_0x2293a9,groupAdmins:_0xda60fc,isBotAdmins:_0xc6af90,isAdmins:_0x15c48b,reply:_0x3e6e02})=>{const _0x5e4408=_0x7aaa28;try{const _0x47373f=_0x1a4278[0x0];if(!_0x47373f)return _0x3e6e02(_0x5e4408(0x127));const _0x2d541f=_0x5e4408(0x128)+_0x47373f,_0x41b772=await axios[_0x5e4408(0x13a)](_0x2d541f),_0x108e62=_0x41b772[_0x5e4408(0x125)];let _0x579778=_0x5e4408(0x132)+(_0x108e62[_0x5e4408(0x116)]||_0x108e62[_0x5e4408(0x131)])+_0x5e4408(0x12a)+_0x108e62['html_url']+')\x0a\x0a*│\x20\x20◦*\x20📝\x20*ʙɪᴏ*:\x20'+(_0x108e62['bio']||'Not\x20available')+'\x0a\x0a*│\x20\x20◦*\x20🏙️\x20*ʟᴏᴄᴀᴛɪᴏɴ*:\x20'+(_0x108e62[_0x5e4408(0x130)]||_0x5e4408(0x11e))+_0x5e4408(0x11a)+_0x108e62[_0x5e4408(0x118)]+_0x5e4408(0x11b)+_0x108e62[_0x5e4408(0x135)]+_0x5e4408(0x139)+_0x108e62[_0x5e4408(0x124)]+_0x5e4408(0x120)+new Date(_0x108e62['created_at'])[_0x5e4408(0x119)]()+_0x5e4408(0x123)+_0x108e62[_0x5e4408(0x12b)]+'\x0a\x0a*│\x20\x20◦*\x20*MADE\x20♥\x20BY\x20SILENT-SOBX-MD*\x0a*╰┈───────────────•*\x0a\x0a*•────────────•⟢*\x0a>\x20©\x20ᴘᴏᴡᴇʀᴇᴅ\x20ʙʏ\x20sɪʟᴇɴᴛʟᴏᴠᴇʀ⁴³²\x0a*•────────────•⟢*\x0a';await _0x5246ba[_0x5e4408(0x117)](_0x2b945c,{'image':{'url':_0x108e62[_0x5e4408(0x115)]},'caption':_0x579778},{'quoted':_0xdc92af});}catch(_0x842166){console[_0x5e4408(0x12d)](_0x842166),_0x3e6e02(_0x5e4408(0x129)+(_0x842166['response']?_0x842166[_0x5e4408(0x11f)][_0x5e4408(0x125)][_0x5e4408(0x138)]:_0x842166['message']));}});
+
+const axios = require('axios');
+const config = require('../config');
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "githubstalk",
+    desc: "Fetch detailed GitHub user profile including profile picture.",
+    category: "other",
+    react: "📚",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const username = args[0];
+        if (!username) {
+            return reply("Please provide a GitHub username.");
+        }
+
+        const apiUrl = `https://api.github.com/users/${username}`;
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+
+        let userInfo = `     👨‍💻*SHEIKH-ALI-AHMAD GITSTALK*👨‍💻
+        
+👤 *ᴜꜱᴇʀ ɴᴀᴍᴇ*: ${data.name || data.login}
+
+🔗 *ɢɪᴛʜᴜʙ ᴜʀʟ*:(${data.html_url})
+
+📝 *ʙɪᴏ*: ${data.bio || 'Not available'}
+
+🏙️ *ʟᴏᴄᴀᴛɪᴏɴ*: ${data.location || 'Unknown'}
+
+📊 *ᴘᴜʙʟɪᴄ ʀᴇᴘᴏ*: ${data.public_repos}
+
+👥 *ꜰᴏʟʟᴏᴡᴇʀꜱ*: ${data.followers} | Following: ${data.following}
+
+📅 *ᴄʀᴇᴀᴛʀᴅ ᴅᴀᴛᴇ*: ${new Date(data.created_at).toDateString()}
+
+🔭 *ᴘᴜʙʟɪᴄ ɢɪꜱᴛꜱ*: ${data.public_gists}
+
+*MADE ♥ BY SHEIKH-ALI-AHMAD*
+`;
+
+        await conn.sendMessage(from, { image: { url: data.avatar_url }, caption: userInfo }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`Error fetching data🤕: ${e.response ? e.response.data.message : e.message}`);
+    }
+});
